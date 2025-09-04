29e4500b66494402260a6db6e6141329c6febf47
Derlys Dominguez <derlys275@gmail.com>
2025-09-04 10:58:45 +0100

    add: content in about

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-                <div className={" align-middle rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"}>
-                    <Button>Add task</Button>
-                </div>
-
-    </div>
+      <div className="overflow-hidden bg-grey-900 py-24 sm:py-32">
+          Aquí va mis notas
+      </div>
   );
 }
